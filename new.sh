337209f9db0f5b0199fc11b3592f3f58239a8643
george4n <george4n@gmail.com>
2019-11-21 16:49:22 +0200

    bug fixes

--- new.sh
+++ new.sh
@@ -48,3 +48,87 @@
   group: "televinken"
   password: "234324"
   shell: /bin/bash
+- username: "1"
+  comment: "1 1"
+  group: "1"
+  password: "1"
+  shell: /bin/bash
+- username: "2"
+  comment: "2 2"
+  group: "2"
+  password: "2"
+  shell: /bin/bash
+- username: "3"
+  comment: "3 3"
+  group: "3"
+  password: "3"
+  shell: /bin/bash
+- username: ""
+  comment: "George Neo"
+  group: "televinken"
+  password: "deidijed"
+  shell: /bin/bash
+- username: ""
+  comment: "George Neophytou"
+  group: "501"
+  password: "939834"
+  shell: /bin/bash
+  comment: "George Neophytou"
+  group: "501"
+  password: "303494"
+  shell: /bin/bash
+  comment: "George Neophytou"
+  group: "501"
+  password: "39394"
+  shell: /bin/bash
+  comment: "George Neophytou"
+  group: "televinken"
+  password: "234234"
+  shell: /bin/bash
+  comment: "george Neophytou"
+  group: "televinken"
+  password: "342342"
+  shell: /bin/bash
+  comment: "George Neophytou"
+  group: "televinken"
+  password: "234324"
+  shell: /bin/bash
+users:
+- username: "test1"
+  comment: "John Doe"
+  uid: "1011"
+  group: "televinken"
+  password: "hello2"
+  shell: /bin/bash
+
+- username: "jsmith"
+  comment: "John Smith"
+  uid: "1005"
+  group: "televinken"
+  password: "123456"
+  shell: /bin/bash
+
+
+- username: "gsmith"
+  comment: "George Smith"
+  uid: "1007"
+  group: "televinken"
+  password: "123456"
+  shell: /bin/bash
+
+
+- username: "tuser"
+  comment: "Test User"
+  uid: "1009"
+  group: "televinken"
+  password: "123456"
+  shell: /bin/bash
+
+
+- username: "jmanchild"
+  comment: "Jake Manchild"
+  uid: "1012"
+  group: "3kundservice"
+  password: "123456"
+  shell: /bin/bash
+
