ccf3bbb6efe165b6a528ec430642d2e0264b1c21
george4n <george4n@gmail.com>
2019-11-22 14:09:59 +0200

    update

--- user_del.sh
+++ user_del.sh
@@ -8,15 +8,16 @@ read USERNAME
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     echo "Using sed for Linux"
-        sed -e '/'"$USERNAME"'/{N;N;N;N;N;N;N;d}' roles/users-add/vars/main.yml
+        sed -e '/"tseven"/{N;N;N;N;N;d;}' roles/users-add/vars/main.yml
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "Using sed for Mac OSX"
+        sed -e '/"tseven"/{N;N;N;N;N;d;}' roles/users-add/vars/main.yml
 else
         echo "Unknown OS"
 fi
 
 echo "deleting user..."
-ansible-playbook modify-users.yml -K
+#ansible-playbook modify-users.yml -K
 #git add .
 #git commit -a -m "deleted user $USERNAME"
 #git push
