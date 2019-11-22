9d7850fb7ee8e1e82ae0c35cde3a9e8b6803f224
george4n <george4n@gmail.com>
2019-11-22 15:14:04 +0200

    update

--- user_del.sh
+++ user_del.sh
@@ -13,7 +13,7 @@ function delete-user-conf-osx {
 function delete-user-conf-linux {
 
     echo -e "\n- $USERNAME" | tee -a $USERDELFILE
-    sed -i '/"'$USERNAME'"/{N;N;N;N;N;d}' $USERADDFILE
+    sed -i '/"'$USERNAME'"/{N;N;N;N;N;N;d;}' $USERADDFILE
 
 }
 
@@ -33,8 +33,38 @@ echo "Enter the username to delete:"
 read USERNAME
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo "Using Linux OS"
-        sed -e '/"'$USERNAME'"/{N;N;N;N;N;d}' roles/users-add/vars/main.yml
+        echo "Using Linux OS"
+        
+        #Find patten and print line
+
+        if grep -q $USERNAME $USERADDFILE; then
+            echo "$USERNAME found!"
+            sed -n  '/"'$USERNAME'"/{N;N;N;N;N;p;}' $USERADDFILE
+                
+                # Ask permission to delete user
+                read -p "-------> Do you want to delete this user? The operation is NOT reversible(y/n) " -n 1 -r
+                    
+                    
+
+                if [[ ! $REPLY =~ ^[Yy]$ ]]
+                then
+                    clear
+                    echo "Exiting: Rerun the script"
+                    exit 1
+                fi
+
+                clear
+                    delete-user-conf-linux
+                    playbook
+                    exit 0
+        else
+            echo "$USERNAME not found!"
+            echo "aborting..."
+            exit 0
+        fi
+
+
+
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         
         echo "Using Mac OSX"
