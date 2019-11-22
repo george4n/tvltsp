5e905b1a789f1d7d364ec1eeb7c4b8269a196e16
george4n <george4n@gmail.com>
2019-11-22 13:41:04 +0200

    update

--- user_del.sh
+++ user_del.sh
@@ -1,2 +1,28 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        sed -e '/'"$USERNAME"'/{N;N;N;N;N;N;N;d}' vars.yml
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+elif [[ "$OSTYPE" == "win32" ]]; then
+        # I'm not sure this can happen.
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+else
+        # Unknown.
+fi
 
 
+echo "Username:"
+read USERNAME
+
+echo "deleting user..."
+ansible-playbook modify-users.yml -K
+#git add .
+#git commit -a -m "deleted user $USERNAME"
+#git push
+echo "user deletion complete!"
